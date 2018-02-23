e6902b286fd527bc4ebf43424909f5ae9252d0ef
Fanb <fanb@stpass.com>
2018-02-23 17:27:05 +0800

    获取bs4 转换的对象

---
+++ element-data-py/bsShop.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
+import urllib
+import re
+import time
+import socket
+import jsonShop
+
+def getBsObj (url):
+	browser = webdriver.PhantomJS()
+	browser.get(url)
+	time.sleep(10)
+	bsObj = BeautifulSoup(browser.page_source, "html.parser")
+	return bsObj
+
+def getBsObjJs (url, js):
+	browser = webdriver.PhantomJS()
+	browser.get(url)
+	time.sleep(10)
+	print('js 执行开始')
+	# js = "document.body.scrollTop=2400"
+	# js = "var imgBox = document.getElementsByClassName('menucategory-categoryItem_3e27M'); var e = document.createEvent('MouseEvents'); e.initEvent('click', true, true); for (var i = 0; i < imgBox.length; i++) {(function(j){ setTimeout(function () {imgBox[j].dispatchEvent(e)}, 5000 * j)})(i);}"
+	browser.execute_script(js)
+	time.sleep(80)
+	print('js 执行结束')
+	bsObj = BeautifulSoup(browser.page_source, "html.parser")
+	return bsObj
